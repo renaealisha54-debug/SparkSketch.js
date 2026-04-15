2d40c7018cd9adec97b64895775719c3eb7d48b3
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-15 18:06:47 -0400

    Delete Structure.js

--- Structure.js
+++
@@ -1,8 +0,0 @@
-// Shared Structure Definition
-export interface ProjectFile {
-  id: string;
-  name: string;   // e.g., 'index.html', 'styles.css'
-  content: string;
-  type: 'html' | 'js' | 'css' | 'tsx';
-  lastModified: number;
-}
